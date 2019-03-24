047dc3643b0745b6258969ee120e48b46980f0dd
okitakagura <354207983@qq.com>
2019-03-24 14:33:05 +0800

    initial

---
+++ 人脸对比-3/Assets/Myslide.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum Axis
+{
+    X,
+    Y,
+    Z
+}
+public class Myslide : MonoBehaviour
+{
+    public GameObject target;//要移动的物体
+    public Slider slider;//slider组件
+    public Axis axis = Axis.X;//按照此轴进行移动
+    public float left_bottom_back = 0;//左边界
+    public float right_up_forward = 1;//右边界
+    private float D_value;//左右边界的差值
+
+    void Start()
+    {
+        D_value = right_up_forward - left_bottom_back;
+        //以下是将物体的初始位置设为slider的初始位置
+        switch (axis)
+        {
+            case Axis.X:
+                target.transform.localPosition += new Vector3(-target.transform.localPosition.x + left_bottom_back + D_value * slider.value, 0, 0);
+                break;
+            case Axis.Y:
+                target.transform.position += new Vector3(0, -target.transform.position.y + left_bottom_back + D_value * slider.value, 0);
+                break;
+            case Axis.Z:
+                target.transform.position += new Vector3(0, 0, -target.transform.position.z + left_bottom_back + D_value * slider.value);
+                break;
+        }
+
+        slider.onValueChanged.AddListener(delegate { this.handleValuChange(); });
+        if (!target)
+        {
+            Debug.Log("missing target!");
+        }
+        if (!slider)
+        {
+            Debug.Log("missing slider!");
+        }
+    }
+
+    public void handleValuChange()
+    {
+        //当slider的value改变时，调用这个函数，并改变物体的位置
+        switch (axis)
+        {
+            case Axis.X:
+                float Xpos = left_bottom_back + D_value * slider.value;
+                target.transform.localPosition += new Vector3(-target.transform.localPosition.x + Xpos, 0, 0);
+                break;
+            case Axis.Y:
+                float Ypos = left_bottom_back + D_value * slider.value;
+                target.transform.position += new Vector3(0, -target.transform.position.y + Ypos, 0);
+                break;
+            case Axis.Z:
+                float Zpos = left_bottom_back + D_value * slider.value;
+                target.transform.position += new Vector3(0, 0, -target.transform.position.z + Zpos);
+                break;
+        }
+    }
+}
---
+++ 人脸对比-3/Assets/Scripts/blink.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blink : MonoBehaviour
+{
+    public float left_bottom_back = -0.25f;//左边界
+    public float right_up_forward = -0.17f;//右边界
+    float speed = 5;
+    // Start is called before the first frame update
+    public GameObject Obj1;
+    int d1, d2;
+    void Start()
+    {
+
+        //d2 = script.state2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Obj1 = GameObject.Find("GameManager");
+        FaceDetect script = Obj1.GetComponent<FaceDetect>();
+        d1 = script.state1;
+        if (d1 == 1)
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, transform.localPosition + new Vector3(-transform.localPosition.x + (-0.25f), 0, 0), speed * Time.deltaTime);
+        }
+        if (d1 == 0)
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, transform.localPosition + new Vector3(-transform.localPosition.x + (-0.17f), 0, 0), speed * Time.deltaTime);
+        }
+        Debug.Log("blink.d1:" + d1);
+        // transform.localPosition += new Vector3(-transform.localPosition.x  + (-0.17f), 0, 0);
+    }
+
+
+}
